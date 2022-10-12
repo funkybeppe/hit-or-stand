676f07d679f834ee396b1ae7476503d95bf01be3
funkybeppe <lucabarbero1996@gmail.com>
2022-10-12 15:58:28 +0000

    Import randon and input modules and create card class for deck

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
+# To generate random cards
 import random
-import os
-import time
+# To allow user input control
+import pyinputplus as pyip
 
 
+class Card:
+
+    def __init__(self, suit, value, card_value):
+         
+        # Suit of the Card like Spades and Clubs
+        self.suit = suit
+ 
+        # Representing Value of the Card like A for Ace, K for King
+        self.value = value
+ 
+        # Score Value for the Card like 10 for King
+        self.card_value = card_value
+
+def print_cards(cards, hidden):
+    
