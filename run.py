63af5a08d4635291bb4b764b70ab20f21216ec04
funkybeppe <lucabarbero1996@gmail.com>
2022-10-13 10:05:21 +0000

    In case dealers scores 21 and gets blackjack, game ends

--- run.py
+++ run.py
@@ -458,3 +458,16 @@ The bartenders are efficient around here.
 
         elif anothergame == "yes":
             blackjack_game(deck)
+
+    # Dealer gets a blackjack
+    if dealer_score == 21:
+        print("DEALER HAS A BLACKJACK!!! {} LOSES".format(player1.upper()))
+
+        # User input validation for another game question
+        anothergame = pyip.inputYesNo(prompt="Want to try another round?(Y/N) ")
+        if anothergame == "no":
+            print("Thanks for playing! Come back for free drinks anytime!")
+            quit()
+
+        elif anothergame == "yes":
+            blackjack_game(deck)
