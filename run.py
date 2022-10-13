3ad7ba1fcb16d14f0ab15c086e4d9e35fda8ba52
funkybeppe <lucabarbero1996@gmail.com>
2022-10-13 10:57:07 +0000

    Minor changes to rules section allowing a better fit in heroku terminal

--- run.py
+++ run.py
@@ -160,7 +160,7 @@ def blackjack_game(deck):
     
     # In case user selected Yes in the previous input
     if drink == "yes":
-        drink_choice = pyip.inputNum(prompt="What kind of drink would you like? eg.(1,2,3)\n 1. Beer  2. Wine  3. Cocktail\n   ", min=1, lessThan=4)
+        drink_choice = pyip.inputNum(prompt="What kind of drink would you like? eg.(1,2,3)\n 1. Beer  2. Wine  3. Cocktail\n  ", min=1, lessThan=4)
 
         # Beer drink choice
         if drink_choice == 1:
@@ -231,10 +231,10 @@ The bartenders are efficient around here.
 
     # In case user selected No in the previous input
     if rules == "no":
-        print("Okay! The rules are simple. You and the dealer will receive two cards in your hand. \n"
-              "Your goal is to beat the dealer by getting closer to 21.\nIf you and the dealer have the same number, it's a tie or push \n"
-              "Cards 2 - 10 counts as it's own number; jacks, queens, and kings counts as 10 each, and aces can count\n"
-              "as either 1 or 11. You can draw up to four cards at this table.")
+        print("Okay! The rules are simple.\n You and the dealer will receive two cards in your hand.\n"
+              "Your goal is to beat the dealer by getting closer to 21.\nIf you and the dealer have the same number, it's a tie or push.\n"
+              "Cards 2 - 10 counts as it's own number; jacks, queens, and kings counts as 10 each.\n Aces can count"
+              "as either 1 or 11.\n You can draw up to four cards at this table.")
         print("-"*40)
         print("Let's begin blackjack!")
         print("-"*40)
